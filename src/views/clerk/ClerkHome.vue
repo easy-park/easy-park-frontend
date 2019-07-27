b0d180f92db5e82d12df89a8052bee8556cc0516
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-27 15:26:36 +0800

    [Amy] init: create component OrderList.vue

--- src/views/clerk/ClerkHome.vue
+++ src/views/clerk/ClerkHome.vue
@@ -1,41 +1,99 @@
 <template>
   <a-layout class="clerk-home">
-    <a-tabs class="tabs" :tabBarGutter="0" defaultActiveKey="1" tabPosition="bottom">
+    <a-row id="header">{{ headName }}</a-row>
+    <a-tabs class="tabs" :tabBarGutter="0" defaultActiveKey="0" tabPosition="bottom">
       <a-tab-pane key="0">
         <div slot="tab" class="tab-slot">
           <a-icon type="home"/>
           <span>接单</span>
         </div>
-        <div class="content">Content</div>
+        <div class="content">
+          <OrderList :btnName="() => '抢单'" :orders="orders"
+            :btnCallback = "getOrder"/>
+        </div>
       </a-tab-pane>
       <a-tab-pane key="1">
         <div slot="tab" class="tab-slot">
           <a-icon type="swap"/>
           <span>取车</span>
         </div>
-        <div class="content">Content</div>
+        <div class="content">
+          <OrderList :btnName="(order) => order.status" :orders="orders1"
+             :btnCallback = "fetchCar" />
+        </div>
       </a-tab-pane>
       <a-tab-pane key="2">
         <div slot="tab" class="tab-slot">
           <a-icon type="bars"/>
           <span>历史</span>
         </div>
-        <div class="content">Content</div>
+        <div class="content">
+          <OrderList :btnName="() => '详细信息'" :orders="orders2"
+             :btnCallback = "orderInfo" />
+        </div>
       </a-tab-pane>
       <a-tab-pane key="3">
         <div slot="tab" class="tab-slot">
           <a-icon type="user"/>
           <span>我的</span>
         </div>
-        <div class="content">Content</div>
+        <div class="content">Content3</div>
       </a-tab-pane>
     </a-tabs>
   </a-layout>
 </template>
 
 <script>
+import OrderList from '@/components/clerk/OrderList'
 export default {
-  name: 'clerk-home'
+  name: 'clerk-home',
+  components: { OrderList },
+  data () {
+    return {
+      headName: '订单',
+      orders1: [
+        {
+          carNumber: '1234',
+          startTime: '12:23',
+          endTime: '',
+          status: '已停'
+        },
+        {
+          carNumber: '1234',
+          startTime: '12:23',
+          endTime: '',
+          status: '待取'
+        }
+      ],
+      orders: [
+        {
+          carNumber: '1234',
+          startTime: '12:23'
+        },
+        {
+          carNumber: '1234',
+          startTime: '12:23'
+        }
+      ],
+      orders2: [
+        {
+          carNumber: '1234',
+          startTime: '12:23',
+          endTime: '15:35'
+        },
+        {
+          carNumber: '1234',
+          startTime: '12:23',
+          endTime: '15:35'
+        }
+      ]
+    }
+  },
+  methods: {
+    getOrder (order) {},
+    fetchCar (order) {},
+    orderInfo (order) {}
+  }
 }
 </script>
 
@@ -81,5 +139,13 @@ export default {
 
 /deep/ .ant-tabs-nav .ant-tabs-tab {
   flex: 1 1 auto;
+}
+#header{
+  text-align: center;
+  height: 50px;
+  line-height: 50px;
+  background: #1890ff;
+  color: white;
+  font-size: 18px;
 }
 </style>
