185c83aeb04ce752e381d948ea3716b06ced77a8
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-28 15:09:49 +0800

    [Amy] fixed: update orderDetail, solve date display format prolem

--- src/views/clerk/OrderDetail.vue
+++ src/views/clerk/OrderDetail.vue
@@ -10,11 +10,11 @@
         </a-row>
         <a-row>
             <a-col :span="6">开始时间：</a-col>
-            <a-col :span="18">{{ order.startTime }}</a-col>
+            <a-col :span="18">{{ order.startTime | toDate }}</a-col>
         </a-row>
         <a-row>
             <a-col :span="6">结束时间：</a-col>
-            <a-col :span="18">{{ order.endTime }}</a-col>
+            <a-col :span="18">{{ order.endTime | toDate }}</a-col>
         </a-row>
         <a-row>
             <a-col :span="6">停车员：</a-col>
@@ -24,8 +24,14 @@
 </template>
 
 <script>
+import { formatDate } from '@/util/datetime'
 export default {
   name: 'orderDetail',
-  props: ['order']
+  props: ['order'],
+  filters: {
+    toDate (datetimeString) {
+      return datetimeString ? formatDate(new Date(datetimeString), 'YYYY-MM-DD hh:mm') : ''
+    }
+  }
 }
 </script>
