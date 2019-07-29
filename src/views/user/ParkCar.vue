1e4e372e6fc3ee1287a2d968e6df124f2bff86ad
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-29 15:07:38 +0800

    [Amy] init: add user park car

---
+++ src/views/user/ParkCar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="parkCar">
+    <a-input placeholder="请输入车牌号" type="text" v-model="carNumber"/>
+    <a-button type="primary" style="margin-top: 20px" @click="parkCar">停车</a-button>
+    <a-model v-model="visible" title="订单信息" :footer="null" destroyOnClose>
+      <order-detail :order='order'></order-detail>
+    </a-model>
+  </div>
+</template>
+
+<script>
+import { parkCar } from '@/api/user/user-order'
+export default {
+  name: 'parkCar',
+  data () {
+    return {
+      carNumber: '',
+      visible: false,
+      order: {}
+    }
+  },
+  methods: {
+    parkCar () {
+      parkCar(this.carNumber).then(res => {
+        this.visible = true
+        this.order = res.data
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+#parkCar {
+  height: 100vh;
+  text-align: center;
+  margin: 30vh 20vw;
+}
+</style>
---
+++ src/views/user/UserHome.vue
@@ -0,0 +1,115 @@
+<template>
+  <a-layout class="user-home">
+    <a-row id="header">{{ title }}</a-row>
+    <a-tabs class="tabs" :tabBarGutter="0" :defaultActiveKey="defaultActiveKey" tabPosition="bottom" @change="onTabChange">
+      <a-tab-pane key="0">
+        <div slot="tab" class="tab-slot">
+          <a-icon type="home"/>
+          <span>停车</span>
+        </div>
+        <div class="content">
+          <ParkCar />
+        </div>
+      </a-tab-pane>
+      <a-tab-pane key="1">
+        <div slot="tab" class="tab-slot">
+          <a-icon type="swap"/>
+          <span>取车</span>
+        </div>
+        <div class="content">
+          <FetchCar />
+        </div>
+      </a-tab-pane>
+      <a-tab-pane key="2">
+        <div slot="tab" class="tab-slot">
+          <a-icon type="bars"/>
+          <span>历史</span>
+        </div>
+        <div class="content">
+        </div>
+      </a-tab-pane>
+      <a-tab-pane key="3">
+        <div slot="tab" class="tab-slot">
+          <a-icon type="user"/>
+          <span>我的</span>
+        </div>
+        <div class="content">
+        </div>
+      </a-tab-pane>
+    </a-tabs>
+  </a-layout>
+</template>
+
+<script>
+import ParkCar from './ParkCar'
+import FetchCar from './FetchCar'
+const TITLES = ['停车', '取车', '历史', '我的']
+export default {
+  name: 'user-home',
+  components: { ParkCar, FetchCar },
+  data () {
+    return {
+      defaultActiveKey: '0',
+      title: TITLES[0]
+    }
+  },
+  methods: {
+    onTabChange (key) {
+      this.title = TITLES[key]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#header{
+  text-align: center;
+  height: 50px;
+  line-height: 50px;
+  background: #1890ff;
+  color: white;
+  font-size: 18px;
+}
+.user-home {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.tabs /deep/ .ant-tabs-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.content {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  padding: 0 0 65px;
+}
+
+.tab-slot {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
+
+.tab-slot i {
+  margin: 0 !important;
+  font-size: 20px;
+}
+
+/deep/ .ant-tabs-nav {
+  width: 100%;
+}
+
+/deep/ .ant-tabs-nav > div {
+  display: flex;
+}
+
+/deep/ .ant-tabs-nav .ant-tabs-tab {
+  flex: 1 1 auto;
+}
+</style>
